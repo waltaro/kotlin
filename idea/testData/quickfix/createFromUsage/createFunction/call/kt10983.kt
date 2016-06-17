cc917f57dc06fcad1ca17c31b7e78824158baa82
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-06-17 15:19:45 +0300

    Refactor resolution of double colon expression LHS
    Try resolving the LHS only when it looks (PSI-wise) like it could represent a
    type element. This, for example, allows "illegal selector" error to be reported
    on weird expressions like '""?.""::class'.
    
    Also remove expression text from the "illegal selector" diagnostic, it's not
    needed and can screw up the error message if the text is too big

--- idea/testData/quickfix/createFromUsage/createFunction/call/kt10983.kt
+++ idea/testData/quickfix/createFromUsage/createFunction/call/kt10983.kt
@@ -2,7 +2,7 @@
 // ERROR: A 'return' expression required in a function with a block body ('{...}')
 // ERROR: Cannot infer a type for this parameter. Please specify it explicitly.
 // ERROR: Cannot infer a type for this parameter. Please specify it explicitly.
-// ERROR: Expression 'return groupsByLength.values.firstOrNull { group -> {group.size == maximumSizeOfGroup} }' cannot be a selector (occur after a dot)
+// ERROR: The expression cannot be a selector (occur after a dot)
 // ERROR: Unresolved reference: groupBy
 // ERROR: Unresolved reference: it
 // ERROR: Unresolved reference: maximumSizeOfGroup
